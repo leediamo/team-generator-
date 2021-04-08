2138ceb58352e260abc45b2c3246a0f89196e607
Diamond Lee <diamolee55@gmail.com>
2021-04-08 00:09:39 -0400

    still figuring it out

--- app.js
+++ app.js
@@ -6,11 +6,38 @@ const util = require('util');
 
 //reader and writer lol
 const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile); 
+const writeFile = util.promisify(fs.writeFile);
 
 //Pulling other JS files (classes)
-const Engineer = require("./team-js/engineer");
-const Employee = require("./team-js/employee");
-const Intern = require("./team-js/intern");
-const Manager = require("./team-js/manager");
+const Engineer = require("./team-js/Engineer");
+const Intern = require("./team-js/Intern");
+const Manager = require("./team-js/Manager");
 
+// Questionaire
+    inquirer
+        .prompt([
+            {
+                message: "What's your name?",
+                name: "name"
+            },
+            {
+                type: "list",
+                message: "Select a role.",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Manager"
+                ],
+                name: "role"
+            },
+            {
+                message: "Enter team member's id",
+                name: "id"
+            },
+            {
+                message: "Enter team member's email address",
+                name: "email"
+            }])
+
+
+    
